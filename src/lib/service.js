99aba44950a87bfb1c78375ecda8ecee3cd8dae8
Esra Cagnur <esracagnurcolak@gmail.com>
2020-12-25 23:25:15 +0300

    init

---
+++ src/lib/service.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+import axios from 'axios';
+
+const ENDPOINT = 'https://jsonplaceholder.typicode.com';
+
+const getData = async (userId) => {
+  console.log({ userId });
+  try {
+    const { data: users } = await axios.get(`${ENDPOINT}/users/${userId}`);
+    const { data: posts } = await axios.get(`${ENDPOINT}/posts/${userId}`);
+    return { ...users, ...posts };
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default getData;
