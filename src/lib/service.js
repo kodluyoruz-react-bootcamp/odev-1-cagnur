7bbd886d1603b82cd2f3da5d79b7cdb0b506bd3b
cagnur <esracagnurcolak@gmail.com>
2020-12-25 23:27:24 +0300

    deleted console.log

--- src/lib/service.js
+++ src/lib/service.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 const ENDPOINT = 'https://jsonplaceholder.typicode.com';
 
 const getData = async (userId) => {
-  console.log({ userId });
   try {
     const { data: users } = await axios.get(`${ENDPOINT}/users/${userId}`);
     const { data: posts } = await axios.get(`${ENDPOINT}/posts/${userId}`);
